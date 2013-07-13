3540619d90eeb5564eee4df276a3d12ef932273e
varu01234 <varadhan198@gmail.com>
2013-07-13 13:04:40 +0530

    modified:   Project/About.php 	new file:   Project/DatabaseLogo.jpg 	new file:   Project/about.jpg 	new file:   Project/back.jpg 	new file:   Project/final.png 	new file:   Project/final1.png 	new file:   Project/logo.jpg 	new file:   Project/logo.png 	new file:   Project/logo_icon.png 	new file:   Project/ologo.jpg 	new file:   Project/ologo1 (2).jpg 	new file:   Project/ologo1.jpg 	new file:   Project/ologo2.png 	new file:   Project/style/back1.jpg 	new file:   Project/style/back2.jpg 	new file:   Project/style/back3.jpg 	new file:   Project/style/back4.jpg 	new file:   Project/style/back5.jpg 	new file:   Project/style/back6.jpg 	new file:   Project/style/back6.png 	new file:   Project/style/back7.jpg 	new file:   Project/style/back8.jpg 	new file:   Project/style/back9.jpg 	new file:   Project/video.php

--- Project/About.php
+++ Project/About.php
@@ -30,7 +30,15 @@
 			<img src="images/divide.png"/><br />
 	<tr>
 		<td>
-			<a href="https://www.facebook.com/varadhan.ramamoorthy.1"><img src="about.jpg"/></a>
+			<?php													//used gravatar for my blog
+				$email = "varadhan198@gmail.com";
+				$default = "http://en.gravatar.com/userimage/52673634/dee1fc6f391b9e90a14e7d24a05235d1.jpg";
+				$size =200;
+				$grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
+				?>
+				<div style="color:white; ">
+				<img src="<?php echo $grav_url; ?>" alt="Writers Blog" />
+				</div>
 	<tr>
 		<td><font face='verdana'>Hi this is R.VARADHAN. Have created this blog for those people who want to express their views and thoughts to other people of same interest.Their views may include problems that are faced by common people in day-to-day life.So you can send those views as an article to this mail <font color=red><u>varadhan198@gmail.com</u></font> which will be published on behalf of you.Include your name and the contents of your views.</face>			
 </table>
---
+++ Project/video.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<link rel="icon" 
+			type="image/jpg" 
+			href="logo.jpg" />
+			<title>Videos</title>
+<link href="style/main.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+
+.style1 {font-size: 35px}
+.style3 {font-size: 20px}
+.style4 {font-size: 15px}
+.style5 {font-size: 30px}
+.style6 {font-size: 12px}
+
+</style>
+</head>
+<table cellpadding=10 cellspacing=10 width='650' align=center>
+<tr><td colspan=2 align=center><span class="style1">Videos</span>
+<tr><td colspan=2 align=center><img src="images/divide.png" />
+<tr><td align=right><iframe width="400" height="300" src="//www.youtube.com/embed/YrtANPtnhyg" frameborder="0" allowfullscreen></iframe>
+	<td align=right><iframe width="400" height="300" src="//www.youtube.com/embed/N_m56irWKeI" frameborder="0" allowfullscreen></iframe>
+<tr><td align=right><font color=red face=verdana>Pranav Mistry: The thrilling potential of SixthSense technology</font>
+	<td align=right><font color=red face=verdana>World's Top3 Humanoid Robots - Asimo vs HPR-4 vs NAO!</font>
+</table>
+
+<ul id=menu>
+<li><a href="index.php"><font face=verdana>Home</font></a></li>
+<li><a href="About.php"><font face=verdana>About</font></a></li>
+<li><a href="All.php"><font face=verdana>All Entries</font></a></li>
+<li><a href="poll.php"><font face=verdana>Poll Archives</font></a></li>
+</html>
